6dbab9777f5932648254844e17c1ccd7ce098890
Jcjunior1992 <jcjunior1992@gmail.com>
2024-03-12 21:37:03 -0300

    v2

--- src/Banco.java
+++ src/Banco.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Banco {
+public class Banco {
+
+   private String nome;
+
+   public String getNome() {
+      return nome;
+   }
+
+   public void setNome(String nome) {
+      this.nome = nome;
+   }
 }
--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class ContaPoupança {
+public class ContaPoupanca {
+
+
+
+    private String agencia;
+    private int numero;
+
+    private Double saldo;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public void sacar(){
+
+    }
+    public void depositar(){
+
+    }
+    public void transferir(){
+
+    }
 }
